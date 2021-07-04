729805aaff75b6e494fb910fbbd3fd6ed149cdb5
Roman Chervinko <chervinko@auction-house.ru>
2021-07-04 21:33:12 +0300

    10й урок.

---
+++ src/ru/itmo/lesson/tenthlesson/User.java
@@ -0,0 +1,17 @@
+package ru.itmo.lesson.tenthlesson;
+
+/**
+ * @author chervinko <br>
+ * 04.07.2021
+ */
+public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
